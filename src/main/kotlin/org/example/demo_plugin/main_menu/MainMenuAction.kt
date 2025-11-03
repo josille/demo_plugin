88d402e58696e7fb3b33e0b256a14de40ccefb56
Rodrigo de la Garza Casado <rodrigo.dlg.casado@de-unit-2142.fritz.box>
2025-11-03 14:15:28 +0100

    Add custom main menu group and associated action in plugin.xml with implementation

---
+++ src/main/kotlin/org/example/demo_plugin/main_menu/MainMenuAction.kt
@@ -0,0 +1,16 @@
+package com.example.demo_plugin.main_menu
+
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.ui.Messages
+
+class MainMenuAction : AnAction() {
+    override fun actionPerformed(e: AnActionEvent) {
+        Messages.showMessageDialog(
+            e.project,
+            "Welcome from the MainMenuAction Demo Plugin!",
+            "MainMenuAction Demo Plugin - Welcome",
+            Messages.getInformationIcon()
+        )
+    }
+}
