02d31cb08410c6152b215000c376313abe162459
Rodrigo de la Garza Casado <rodrigo.dlg.casado@de-unit-2142.fritz.box>
2025-12-15 11:52:22 +0100

    Add class-level KDocs for `FileMenuAction` to improve documentation and clarity

--- src/main/kotlin/org/example/demo_plugin/main_menu/FileMenuAction.kt
+++ src/main/kotlin/org/example/demo_plugin/main_menu/FileMenuAction.kt
@@ -4,6 +4,16 @@ import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.ui.Messages
 
+
+/**
+ * Represents an action bound to a file menu in an IntelliJ plugin.
+ * When triggered, it displays a message dialog with a greeting related to the FileMenuAction Demo Plugin.
+ *
+ * This action can be used as part of the IntelliJ platform's main menu actions
+ * to interact with users through simple notifications.
+ *
+ * Implements AnAction abstract class
+ */
 class FileMenuAction : AnAction() {
     override fun actionPerformed(e: AnActionEvent) {
         Messages.showMessageDialog(
